3f171a577d997c22e9b22a46d8aeb16ae0a8c331
Leman Caliskan <jmleman95@gmail.com>
2024-02-24 15:40:01 +0300

    maske takip uygulaması

---
+++ Workaround/Program.cs
@@ -0,0 +1,25 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+namespace Workaround
+{
+    class Program
+    {
+        
+            static void Main(string[] args)
+            {
+                Person person1 = new Person();
+                person1.FirstName = "Leman";
+                person1.LastName = "Çalışkan";
+                person1.NationalIdentity = 01234567891;
+                person1.DateOfBirthYear = 1994;
+                PttManager pttManager = new PttManager(new PersonManager());
+                pttManager.GiveMask(person1);
+                Console.ReadLine();
+
+
+            }
+        }
+    }
+
